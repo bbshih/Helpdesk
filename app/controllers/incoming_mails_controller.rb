eb4d973c1afe2706d957c5911d9ad843bdbc4a36
Billy Shih <me@billyshih.com>
2013-06-06 00:54:52 -0700

    new incoming

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -12,12 +12,18 @@ class IncomingMailsController < ApplicationController
     id = params[:subject][/\d+/].to_i # finds the first number of the subject line
     
 
-    ticket = Ticket.find(id)
-    ticket.responses.create( :body => params[:plain])    
+    @ticket = Ticket.find(id)
+    # ticket.responses.create( :body => params[:plain])    
     
-    # ticket.responses.build    
-    # ticket.responses.create( body: params[:plain])
-    render :text => 'success', :status => 200 # a status of 404 would reject the mail
+    @response = ticket.responses.build( body: params[:plain])    
+    # @ticket = ticket.responses.create( body: params[:plain])
+
+    if @response.save
+      render :text => 'success', :status => 200 # a status of 404 would reject the mail
+    else
+      render :text => 'Internal failure', :status => 501
+    end
+    
   end
 
 end
