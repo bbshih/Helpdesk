6592c2bdd3e0ae90bf0e0f732740c71fbe09fd44
Billy Shih <me@billyshih.com>
2013-06-06 01:12:23 -0700

    Remove logic from incoming

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,22 +2,18 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
+    render :text => 'success', :status => 200 # a status of 404 would reject the mail
+    # id = params[:subject][/\d+/].to_i # finds the first number of the subject line
+    # @ticket = Ticket.find(id)
+    # # ticket.responses.create( :body => params[:plain])    
+    # @response = @ticket.responses.build( body: params[:plain] )    
+    # # @ticket = ticket.responses.create( body: params[:plain])
 
-    
-    id = params[:subject][/\d+/].to_i # finds the first number of the subject line
-    
-
-    @ticket = Ticket.find(id)
-    # ticket.responses.create( :body => params[:plain])    
-    
-    @response = @ticket.responses.build( body: params[:plain])    
-    # @ticket = ticket.responses.create( body: params[:plain])
-
-    if @response.save
-      render :text => 'success', :status => 200 # a status of 404 would reject the mail
-    else
-      render :text => 'Internal failure', :status => 501
-    end
+    # if @response.save
+    #   render :text => 'success', :status => 200 # a status of 404 would reject the mail
+    # else
+    #   render :text => 'Internal failure', :status => 501
+    # end
     
   end
 
