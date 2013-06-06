e316fb6e87c15b5d30b996e0085a938da32daba9
Billy Shih <me@billyshih.com>
2013-06-06 13:59:22 -0700

    params only

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -3,14 +3,14 @@ class IncomingMailsController < ApplicationController
 
   def create
     subject = params[:headers][:Subject]
-    id = subject.match(/\d+$/).to_s
+    # id = subject.match(/\d+$/)
     # id = [/\d+/] # finds the first number of the subject line
-    @ticket = Ticket.find(id)
+    # @ticket = Ticket.find(id)
 
     # doesn't work b/c of fixed parameters?
     # @response = @ticket.responses.build( body: params[:plain] )  
 
-    @ticket.responses.create( body: params[:plain] )
+    # @ticket.responses.create( body: params[:plain] )
 
     # if @response.save
       render :text => 'success', :status => 200 # a status of 404 would reject the mail
