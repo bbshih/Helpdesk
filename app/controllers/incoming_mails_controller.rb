dfac389158b8be57397bd826c0603eac076c24a1
Billy Shih <me@billyshih.com>
2013-06-05 21:49:01 -0700

    Add feature to append response to ticket

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,10 +2,11 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    params[:subject] # print the subject to the logs
-    params[:plain] # print the decoded body plain to the logs if present
+    id = params[:subject][/\d+/].to_i # finds the first number of the subject line
+    @ticket = Ticket.find(id)
+    @ticket.body = "Response: <br>" + params[:plain]
 
-    render :text => 'success', :status => 200 # a status of 404 would reject the mail
+    redirect_to @ticket, :status => 200 # a status of 404 would reject the mail
   end
 
 end
