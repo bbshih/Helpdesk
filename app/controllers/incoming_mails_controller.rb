8b3e3fe309ed2941ad378077da71eaa1ee5d2529
Billy Shih <me@billyshih.com>
2013-06-06 00:49:25 -0700

    changed incoming mail code

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,7 +2,7 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-
+    put "Received mail"
     Rails.logger.log params[:envelope][:to] # print the to field to the logs
     Rails.logger.log params[:subject] # print the subject to the logs
     Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
@@ -10,13 +10,13 @@ class IncomingMailsController < ApplicationController
     Rails.logger.log params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
 
     id = params[:subject][/\d+/].to_i # finds the first number of the subject line
-    Rails.logger.info id
+    
 
     ticket = Ticket.find(id)
-    Rails.logger.info params[:ticket]
+    ticket.responses.create( :body => params[:plain])    
     
-    ticket.responses.build    
-    ticket.responses.create( body: params[:plain])
+    # ticket.responses.build    
+    # ticket.responses.create( body: params[:plain])
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
   end
 
