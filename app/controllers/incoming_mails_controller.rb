f59d25b69e353a95268bb5b0b9bf60de40fc869a
Billy Shih <me@billyshih.com>
2013-06-06 00:08:20 -0700

    only success returns

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,17 +1,9 @@
 class IncomingMailsController < ApplicationController
   def create
     Rails.logger.info params
-    message = Message.new(
-      :to => params[:envelope][:to],
-      :from => params[:envelope][:from],
-      :subject => params[:headers]['Subject'],
-      :body => params[:plain]
-    )
-    if message.save
+
       render :text => 'Success', :status => 200
-    else
-      render :text => message.errors.full_messages, :status => 422, :content_type => Mime::TEXT.to_s
-    end
+
   end
 
 
