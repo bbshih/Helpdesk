e2af481c12fe50bdf69abf5cdacb12b20e554c60
Billy Shih <me@billyshih.com>
2013-06-06 13:41:36 -0700

    Using logic again

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -2,18 +2,18 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    # subject = params[:headers][:Subject]
-    # id = subject.match(/\d+$/).to_s
+    subject = params[:headers][:Subject]
+    id = subject.match(/\d+$/).to_s
     # id = [/\d+/] # finds the first number of the subject line
-    # @ticket = Ticket.find(id)
+    @ticket = Ticket.find(id)
 
     # doesn't work b/c of fixed parameters?
     # @response = @ticket.responses.build( body: params[:plain] )  
 
-    # @ticket.responses.create( body: params[:plain] )
+    @ticket.responses.create( body: params[:plain] )
 
     # if @response.save
-    render :text => 'success', :status => 200 # a status of 404 would reject the mail
+      render :text => 'success', :status => 200 # a status of 404 would reject the mail
     # else
     #   render :text => 'Internal failure', :status => 501
     # end    
